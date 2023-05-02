fc993ded3058d2df7557117f8da6b58ca6994dc6
Sanjay Kumar Gupta <rspvsanjay@gmail.com>
2023-05-03 01:33:53 +0530

    Update training.py

--- training.py
+++ training.py
@@ -8,6 +8,14 @@ from datetime import datetime
 from networks import MIMO_Network
 from dataloader import create_dataset
 
+loss_level1 = tf.keras.metrics.Mean('loss_level1', dtype=tf.float32)
+loss_level2 = tf.keras.metrics.Mean('loss_level2', dtype=tf.float32)
+loss_level3 = tf.keras.metrics.Mean('loss_level3', dtype=tf.float32)
+
+fft_loss_level1 = tf.keras.metrics.Mean('fft_loss_level1', dtype=tf.float32)
+fft_loss_level2 = tf.keras.metrics.Mean('fft_loss_level2', dtype=tf.float32)
+fft_loss_level3 = tf.keras.metrics.Mean('fft_loss_level3', dtype=tf.float32)
+loss_at_level1_epoch = tf.keras.metrics.Mean('loss_at_level1_epoch', dtype=tf.float32)
 
 # set up the training loop
 @tf.function
@@ -16,12 +24,19 @@ def train_step(inputs, targets):
     targets2 = tf.image.resize(targets1, [tf.shape(targets1)[1]//2, tf.shape(targets1)[2]//2])
     targets3 = tf.image.resize(targets2, [tf.shape(targets2)[1]//2, tf.shape(targets2)[2]//2])
 
+    loss1 = 0
+    loss2 = 0
+    loss3 = 0
+
+    fft1_loss = 0
+    fft2_loss = 0
+    fft3_loss = 0
     with tf.GradientTape() as tape3, tf.GradientTape() as tape2, tf.GradientTape() as tape1:
         # forward pass
         pred3, pred2, pred1 = model(inputs, training=True)
         
         # calculate loss of level3
-        loss3 = mse_loss_fn(targets3, pred3)
+        loss3 = mae_loss_fn(targets3, pred3)
         targets3_fft = tf.signal.fft(tf.cast(targets3, tf.complex64))
         preds3_fft = tf.signal.fft(tf.cast(pred3, tf.complex64))
         # calculate FFT loss
@@ -29,7 +44,7 @@ def train_step(inputs, targets):
         loss3 = loss3 + 0.1 * fft3_loss
 
         # calculate loss of level2
-        loss2 = mse_loss_fn(targets2, pred2)
+        loss2 = mae_loss_fn(targets2, pred2)
         targets2_fft = tf.signal.fft(tf.cast(targets2, tf.complex64))
         preds2_fft = tf.signal.fft(tf.cast(pred2, tf.complex64))
         # calculate FFT loss
@@ -37,7 +52,7 @@ def train_step(inputs, targets):
         loss2 = (loss2 + loss3)/2 + 0.1 * fft2_loss
 
         # calculate loss of level1
-        loss1 = mse_loss_fn(targets1, pred1)
+        loss1 = mae_loss_fn(targets1, pred1)
         targets1_fft = tf.signal.fft(tf.cast(targets1, tf.complex64))
         preds1_fft = tf.signal.fft(tf.cast(pred1, tf.complex64))
         # calculate FFT loss
@@ -46,12 +61,18 @@ def train_step(inputs, targets):
 
     grads3 = tape3.gradient(loss3, model.get_layer("convo1k3s1f3l3").trainable_variables) # calculate gradients at level3
     optimizer.apply_gradients(zip(grads3, model.get_layer("convo1k3s1f3l3").trainable_variables)) # update weights
+    loss_level3(loss3)
+    fft_loss_level3(fft3_loss)
 
     grads2 = tape2.gradient(loss2, model.get_layer("convo1k3s1f3l2").trainable_variables) # calculate gradients at level2
     optimizer.apply_gradients(zip(grads2, model.get_layer("convo1k3s1f3l2").trainable_variables)) # update weights
+    loss_level2(loss2)
+    fft_loss_level2(fft2_loss)
     
     grads1 = tape1.gradient(loss1, model.trainable_variables) # calculate gradients at level1
     optimizer.apply_gradients(zip(grads1, model.trainable_variables)) # update weights
+    loss_level1(loss1)
+    fft_loss_level1(fft1_loss)
 
     return loss1
 
@@ -91,7 +112,7 @@ numberofbatch = len(index4)
 #load Networks
 model = MIMO_Network()
 # define the loss function
-mse_loss_fn = tf.keras.losses.MeanSquaredError()
+mae_loss_fn = tf.keras.losses.MeanAbsoluteError()
 
 # define the optimizer with a learning rate schedule
 initial_learning_rate = 1e-4
@@ -100,6 +121,14 @@ optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=lr_schedule)
 # Use the legacy optimizer tf.keras.optimizers.legacy.Adam instead of the tf.keras.optimizers.Adam. 
 # The legacy optimizer does not require the list of variables to be registered separately.
 
+# compile the model with loss and metric functions
+model.compile(optimizer=optimizer, loss=mae_loss_fn, metrics=[tf.keras.metrics.MeanAbsoluteError()])
+
+current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+train_log_dir = '/content/drive/MyDrive/StartCode3/logs/gradient_tape1/' + current_time + '/train'
+train_summary_writer1 = tf.summary.create_file_writer(train_log_dir)
+train_log_dir = '/content/drive/MyDrive/StartCode3/logs/gradient_tape2/' + current_time + '/train'
+train_summary_writer2 = tf.summary.create_file_writer(train_log_dir)
 # set up the checkpoint
 checkpoint_dir = '/content/drive/MyDrive/StartCode3/training_checkpoints'
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
@@ -109,9 +138,7 @@ checkpoint = tf.train.Checkpoint(optimizer=optimizer, model=model)
 epoch_to_restore = 0
 latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
 if latest_checkpoint:
-    # extract the epoch number from the checkpoint file name
-    # checkpoint_prefix = os.path.join(checkpoint_dir, "")
-    match = re.search(r'ckpt_(\d+)', latest_checkpoint)
+    match = re.search(r'ckpt_(\d+)', latest_checkpoint) # extract the epoch number from the checkpoint file name
     if match:
         epoch_to_restore = int(match.group(1))
     print("epoch_to_restore: ", epoch_to_restore)
@@ -122,6 +149,7 @@ log_dir = "/content/drive/MyDrive/StartCode3/logs/fit/" + datetime.now().strftim
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 num_epochs = 4000
+batch_count = 0
 for epoch in range(epoch_to_restore, num_epochs):
     print("Epoch {}/{}".format(epoch+1, num_epochs))
     start_time_epoch = time.time()  # record start time
@@ -136,7 +164,22 @@ for epoch in range(epoch_to_restore, num_epochs):
             end_timeb = time.time()  # record end time 
         end_time = time.time()  # record end time 
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print("Epoch: {} in progress, Loss = {}, Total Number of Mini-batch: {}, Time taken for a mini-batch = {:.2f}s, Batch Number: {}/{}, Time taken for a batch = {:.2f}s, Current Time: {} ".format(epoch+1, loss.numpy(),  numberofminibatch, end_timeb - start_timeb, number1+1, numberofbatch, end_time - start_time, current_time))
+        print("Epoch: {} in progress, Loss = {}, Batch Number: {}/{}, Time taken for a batch = {:.2f}s, Total Number of Mini-batch: {}, Time taken for a mini-batch = {:.2f}s, Current Time: {} ".format(epoch+1, loss.numpy(),  number1+1, numberofbatch, end_time - start_time, numberofminibatch, end_timeb - start_timeb, current_time))
+        with train_summary_writer1.as_default():
+            tf.summary.scalar('loss_level3', loss_level3.result(), step=batch_count)
+            tf.summary.scalar('loss_level2', loss_level2.result(), step=batch_count)
+            tf.summary.scalar('loss_level1', loss_level1.result(), step=batch_count)
+            tf.summary.scalar('fft_loss_level3', fft_loss_level3.result(), step=batch_count)
+            tf.summary.scalar('fft_loss_level2', fft_loss_level2.result(), step=batch_count)
+            tf.summary.scalar('fft_loss_level1', fft_loss_level1.result(), step=batch_count)
+        batch_count = batch_count + 1
+        # Reset metrics every batch
+        fft_loss_level3.reset_states()
+        fft_loss_level2.reset_states()
+        fft_loss_level1.reset_states()
+        loss_level3.reset_states()
+        loss_level2.reset_states()
+        loss_level1.reset_states()
         # create the list of paths
         input_batch_path = []
         output_batch_path = []
@@ -146,18 +189,19 @@ for epoch in range(epoch_to_restore, num_epochs):
         #load data
         train_data = create_dataset(input_batch_path, output_batch_path)
     # add loss to TensorBoard
-    with tf.summary.create_file_writer(log_dir).as_default():
-        tf.summary.scalar('loss', loss, step=epoch)
+    loss_at_level1_epoch(loss)
+    with train_summary_writer2.as_default():
+        tf.summary.scalar('loss_at_level1_epoch', loss_at_level1_epoch.result(), step=epoch)
+    # Reset metrics every epoch
+    loss_at_level1_epoch.reset_states()
     # save checkpoint every 30 epoch
     if (epoch + 1) % 1 == 0:
         checkpoint.save(file_prefix=checkpoint_prefix.format(epoch=epoch))
         print("checkpoint_prefix.format(epoch=epoch): ", checkpoint_prefix.format(epoch=epoch))  
     end_time_epoch = time.time()  # record end time  
-    print("Epoch {} is completed: Loss = {}, Time taken to complete a epoch = {:.2f}s, Batch Number: {}/{},  Current Time: {} ".format(epoch+1, loss.numpy(), end_time_epoch - start_time_epoch, numberofbatch, numberofbatch, current_time))
+    print("Epoch {} is completed: Loss = {}, Batch Number: {}/{}, Time taken to complete a epoch = {:.2f}s, Current Time: {} ".format(epoch+1, loss.numpy(),  numberofbatch, numberofbatch, end_time_epoch - start_time_epoch, current_time))
     random.shuffle(index)
-    # print("shuffled index: ", index)
     index4 = indexing(index, 25)
-    # print("index4 length: ", len(index4))
     input_batch_path = []
     output_batch_path = []
     for num1 in range(len(index4[0])):
@@ -166,4 +210,3 @@ for epoch in range(epoch_to_restore, num_epochs):
     #load data
     train_data = create_dataset(input_batch_path, output_batch_path)
     print("data loaded")
-    # print("data length: ", len(train_data))
